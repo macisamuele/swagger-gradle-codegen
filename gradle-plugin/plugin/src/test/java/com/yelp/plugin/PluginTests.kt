a37227d89c3628ffebfe39ac2461e493e6e6c172
Samuele Maci <macisamuele@gmail.com>
2020-10-31 17:34:46 +0000

    Test all gradle versions

--- gradle-plugin/plugin/src/test/java/com/yelp/plugin/PluginTests.kt
+++ gradle-plugin/plugin/src/test/java/com/yelp/plugin/PluginTests.kt
@@ -10,7 +10,7 @@ import java.io.File
 
 class PluginTests {
     @get:Rule
-    val temporaryFolder = TemporaryFolder(File("."))
+    val temporaryFolder = TemporaryFolder()
 
     @Test
     fun basicPluginTest() {
@@ -34,13 +34,358 @@ class PluginTests {
     }
 
     @Test
-    fun testMinSupportdGradleVersion() {
+    fun testMinSupportdGradleVersion54() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("5.4")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion541() {
         val projectDir = temporaryFolder.newFolder("project")
         File("src/test/testProject").copyRecursively(projectDir)
 
         val result = GradleRunner.create()
             .withProjectDir(projectDir)
             .withGradleVersion("5.4.1")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion55() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("5.5")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion551() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("5.5.1")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion56() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("5.6")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion561() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("5.6.1")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion562() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("5.6.2")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion563() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("5.6.3")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion564() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("5.6.4")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion60() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("6.0")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion601() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("6.0.1")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion61() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("6.1")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion611() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("6.1.1")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion62() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("6.2")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion621() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("6.2.1")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion622() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("6.2.2")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion63() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("6.3")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion64() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("6.4")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion641() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("6.4.1")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion65() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("6.5")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion651() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("6.5.1")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion66() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("6.6")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion661() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("6.6.1")
+            .forwardStdOutput(System.out.writer())
+            .forwardStdError(System.err.writer())
+            .withArguments("generateSwagger")
+            .build()
+
+        Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateSwagger")?.outcome)
+    }
+    @Test
+    fun testMinSupportdGradleVersion67() {
+        val projectDir = temporaryFolder.newFolder("project")
+        File("src/test/testProject").copyRecursively(projectDir)
+
+        val result = GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withGradleVersion("6.7")
             .forwardStdOutput(System.out.writer())
             .forwardStdError(System.err.writer())
             .withArguments("generateSwagger")
