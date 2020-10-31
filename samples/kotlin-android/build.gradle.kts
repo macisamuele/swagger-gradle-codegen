f911a43f23eb2555c98e6adb08376db482621300
Samuele Maci <macisamuele@gmail.com>
2020-10-31 14:43:13 +0000

    Bump okhttp version

--- samples/kotlin-android/build.gradle.kts
+++ samples/kotlin-android/build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     // Moshi + OkHttp + Retrofit
     implementation("com.squareup.moshi:moshi:1.11.0")
     implementation("com.squareup.moshi:moshi-adapters:1.11.0")
-    implementation("com.squareup.okhttp3:okhttp:3.12.12")
+    implementation("com.squareup.okhttp3:okhttp:3.14.9")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
